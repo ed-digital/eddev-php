d734b52ac290bcf5096d37d2a3b5188ac5f15af2
Daniel Lever <daniel@ed.com.au>
2023-09-26 16:01:43 +1000

    Hacks to remove emojis and gutenberg CSS variables

--- core/ed.php
+++ core/ed.php
@@ -54,6 +54,11 @@
       $this->enableDevReact();
 
       edDisableComments();
+      edDisableEmojis();
+
+      remove_action('wp_enqueue_scripts', 'wp_enqueue_global_styles');
+      remove_action('wp_footer', 'wp_enqueue_global_styles', 1);
+      remove_action('wp_body_open', 'wp_global_styles_render_svg_filters');
 
       add_action('wp_head', function() {
         $endpoint = $this->getServerlessEndpoint();
