e841c8f6ea5157a56f164afbec8a7489ff7e56ea
Daniel Lever <daniel@ed.com.au>
2024-11-15 21:14:38 +1100

    Security/meta improvements

--- core/wp-hacks.php
+++ core/wp-hacks.php
@@ -3,11 +3,45 @@
 class EDWPHacks {
 
   static function apply() {
-    self::edDisableComments();
-    self::edDisableEmojis();
+    self::disable_comments();
+    self::disable_emojis();
+    self::disable_xml_rpc();
   }
 
-  static function edDisableComments() {
+  static function disable_xml_rpc() {
+    if (defined('XMLRPC_REQUEST') && constant('XMLRPC_REQUEST') === true) {
+      status_header(403);
+      exit;
+    }
+
+    add_action('init', [__CLASS__, '_disable_all_feeds']);
+  }
+
+  static function _disable_all_feeds() {
+    remove_action('wp_head', 'rsd_link');
+    remove_action('wp_head', 'wlwmanifest_link');
+    remove_action('wp_head', 'feed_links', 2);
+    remove_action('wp_head', 'feed_links_extra', 3);
+    remove_action('wp_head', 'rest_output_link_wp_head');
+    remove_action('wp_head', 'wp_oembed_add_discovery_links');
+    remove_action('template_redirect', 'rest_output_link_header');
+    remove_action('wp_head', 'wp_generator');
+    remove_action('wp_head', 'wp_shortlink_wp_head');
+
+    $feeds = array(
+      'do_feed',
+      'do_feed_rdf',
+      'do_feed_rss',
+      'do_feed_rss2',
+      'do_feed_atom',
+    );
+
+    foreach ($feeds as $feed) {
+      remove_action($feed, $feed);
+    }
+  }
+
+  static function disable_comments() {
     add_action('admin_init', function () {
       // Redirect any user trying to access comments page
       global $pagenow;
@@ -71,15 +105,17 @@ class EDWPHacks {
     }
   }
 
-  static function edDisableEmojis() {
-    add_action('init', function () {
-      remove_action('wp_head', 'print_emoji_detection_script', 7);
-      remove_action('admin_print_scripts', 'print_emoji_detection_script');
-      remove_action('wp_print_styles', 'print_emoji_styles');
-      remove_action('admin_print_styles', 'print_emoji_styles');
-      remove_filter('the_content_feed', 'wp_staticize_emoji');
-      remove_filter('comment_text_rss', 'wp_staticize_emoji');
-      remove_filter('wp_mail', 'wp_staticize_emoji_for_email');
-    });
+  static function disable_emojis() {
+    add_action('init', [__CLASS__, '_disable_emojis']);
+  }
+
+  static function _disable_emojis() {
+    remove_action('wp_head', 'print_emoji_detection_script', 7);
+    remove_action('admin_print_scripts', 'print_emoji_detection_script');
+    remove_action('wp_print_styles', 'print_emoji_styles');
+    remove_action('admin_print_styles', 'print_emoji_styles');
+    remove_filter('the_content_feed', 'wp_staticize_emoji');
+    remove_filter('comment_text_rss', 'wp_staticize_emoji');
+    remove_filter('wp_mail', 'wp_staticize_emoji_for_email');
   }
 }
--- integrations/integration-slim-seo.php
+++ integrations/integration-slim-seo.php
@@ -4,7 +4,6 @@ class SlimSEOIntegration {
   static function init() {
     add_action('slim_seo_init', function ($plugin) {
       $plugin->disable('breadcrumbs');
-      $plugin->disable('schema');
       $plugin->disable('notifications');
       $plugin->disable('feed');
       $plugin->disable('settings_term');
@@ -14,6 +13,10 @@ class SlimSEOIntegration {
       $excerpt = get_the_excerpt($post);
       return $excerpt;
     }, 10, 2);
+
+    add_filter("slim_seo_schema_author_enable", '__return_false');
+    add_filter('slim_seo_meta_author', '__return_false');
+    add_filter('slim_seo_linkedin_author', '__return_false');
   }
 }
 SlimSEOIntegration::init();
