5908d2ded5863063b5c90d680885aa20e6584d1d
Daniel Lever <daniel@ed.com.au>
2025-04-14 17:44:31 +1000

    Add request URL to queries

--- core/rest-queries.php
+++ core/rest-queries.php
@@ -52,6 +52,7 @@ class QueryHandler {
     // Execute the query
     $result = $query->getResult();
     $result['queryMonitor'] = QueryMonitor::getResult();
+    $result['requestUri'] = $_SERVER['REQUEST_URI'];
 
     // Send any required headers
     $query->sendCacheHeaders();
