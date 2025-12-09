49e7bf37ba5e793bcc9155b19c7ae504f5d68eb3
Daniel Lever <daniel@ed.com.au>
2025-12-09 13:42:09 +1100

    Fix favicon thing

--- core/favicon.php
+++ core/favicon.php
@@ -37,13 +37,15 @@ class EDFavicon {
     $config = ED()->getConfig('favicon');
 
     // SVG icon
-    $svgFavicon = self::find([
-      "/assets/favicon-out/favicon.svg",
-      "/favicon.svg",
-      $config['mode'] === 'svg' ? $config['default'] ?? "/assets/favicon/favicon.svg" : null,
-    ]);
-    if ($svgFavicon) {
-      $lines[] = "<link rel=\"icon\" href=\"" . esc_attr($svgFavicon) . "\" />";
+    if (!empty($config) && isset($config['mode'])) {
+      $svgFavicon = self::find([
+        "/assets/favicon-out/favicon.svg",
+        "/favicon.svg",
+        $config['mode'] === 'svg' ? $config['default'] ?? "/assets/favicon/favicon.svg" : null,
+      ]);
+      if ($svgFavicon) {
+        $lines[] = "<link rel=\"icon\" href=\"" . esc_attr($svgFavicon) . "\" />";
+      }
     }
 
     // Regular PNG icons
